c06ac34386c7b1682a8533a0c3859296ea20f721
Wai Phyo <kophyo@nexbe.sg>
2022-04-04 22:06:45 +0630

    forget-setnew-resetcomplete-password

--- components/GlobalStyle/GlobalStyle.js
+++ components/GlobalStyle/GlobalStyle.js
@@ -2,7 +2,6 @@ import { Global, css } from "@emotion/react";
 
 const GlobalStyle = () => {
     return (
-       
         <>
             <Global
                 styles={css`
@@ -14,8 +13,14 @@ const GlobalStyle = () => {
 
                     html {
                         height: 100%;
-                        background-color: #172b4d;
-                        overflow: hidden; 
+                        background: linear-gradient(
+                            90deg,
+                            #2d6db7 0%,
+                            #4d9bb6 52.4%,
+                            #4eafaa 100%
+                        );
+                        background-repeat: no-repeat;
+                        background-attachment: fixed;
                     }
 
                     body {
@@ -24,48 +29,64 @@ const GlobalStyle = () => {
                             Droid Sans, Helvetica Neue, sans-serif;
                         width: 100%;
                         height: 100vh;
-                        background: linear-gradient(
-                            90deg,
-                            #825ee4 0%,
-                            #e6a355 100%
-                        );
-                        background-repeat: no-repeat;
+
                         background-size: 100% 50%;
                     }
 
-                    .fp-left-container {
+                    .fp-left-container,
+                    .snpw-left-container, .prc-left-container {
                         width: 420px;
                         height: 350px;
-                        margin-top: 22%;
-                        margin-bottom: 5%;
+                        display: flex;
+                        flex-direction: column;
+                        justify-content: center;
+                        margin-top: 7.3%;
+                        margin-left: 3%;
+                        user-select: none;
                     }
 
-                    .snwp-left-container {
-                        width: 420px;
-                        height: 350px;
-                        margin-bottom: 6%;
+                    .snpw-left-container {
+                        margin-top: 0;
                     }
 
-                    .ufp-img {
+                    .ufp-img,
+                    .snpw-img {
                         width: 100%;
                     }
 
-                    .fp-right-container {
+                    .fp-right-container,
+                    .snpw-right-container, .prc-right-container {
                         box-sizing: border-box;
                         background-color: #f3f4f6;
-                        margin-top: 2.5%;
-                        margin-bottom: 5%;
                         border: 3px solid #f3f4f6;
                         border-radius: 10px;
+                        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25),
+                            0 10px 10px rgba(0, 0, 0, 0.22);
+                        margin-top: 5.7%;
+                        margin-right: 3%;
                     }
 
-                    .logo , .snwp-logo {
+                    .snpw-right-container {
+                        margin-top: 3.5%;
+                        margin-bottom: 5%;
+                    }
+
+                    .logo,
+                    .snpw-logo {
                         text-align: center;
                         margin-top: 100px;
                         margin-bottom: 15px;
+                        cursor: pointer;
                     }
 
-                    .snwp-logo {
+                    .logo-prc {
+                        text-align: center;
+                        margin-top: 180px;
+                        margin-bottom: 15px;
+                        cursor: pointer;
+                    }
+
+                    .snpw-logo {
                         margin-top: 70px;
                     }
 
@@ -73,7 +94,8 @@ const GlobalStyle = () => {
                         width: 45px;
                     }
 
-                    .fp-text {
+                    .fp-text,
+                    .snpw-text {
                         display: flex;
                         flex-direction: column;
                         align-items: center;
@@ -101,8 +123,6 @@ const GlobalStyle = () => {
                         display: block;
                         font-size: 14px;
                         color: #172b4d;
-
-                        /* margin: 10px; */
                     }
 
                     .fp-text,
@@ -123,7 +143,8 @@ const GlobalStyle = () => {
                         background-color: #ffffff;
                     }
 
-                    .input-with-icon input[type="email"] , input[type="password"] {
+                    .input-with-icon input[type="email"],
+                    input[type="password"] {
                         padding-left: 40px;
                     }
 
@@ -131,13 +152,15 @@ const GlobalStyle = () => {
                         position: relative;
                     }
 
-                    .input-with-icon .fa-env-icon , .fa-unlock-icon {
+                    .input-with-icon .fa-env-icon,
+                    .fa-unlock-icon {
                         position: absolute;
                         left: 0px;
                         top: 15px;
                         padding-left: 15px;
                         font-size: 30px;
                         color: #adb5bd;
+                        cursor: pointer;
                     }
 
                     .input-with-icon .fa-unlock-icon {
@@ -145,26 +168,33 @@ const GlobalStyle = () => {
                         font-size: 28px;
                     }
 
+                    .btn-prc {
+                        width: 335px;
+                        margin-bottom: 180px;
+                    }
+
                     .btn-reset {
                         width: 100%;
                         height: 35px;
-                        border: 2px solid #5e72e4;
+                        border: 2px solid #00abd1;
                         border-radius: 5px;
-                        background-color: #5e72e4;
+                        background-color: #00abd1;
                         color: #ffffff;
                         margin-top: 8px;
                         cursor: pointer;
                     }
 
                     .rq-new-one,
-                    .footer-container, .footer-container-snpw {
+                    .footer-container,
+                    .footer-container-snpw {
                         text-align: center;
                         font-size: 11px;
-                        color: #172B4D;
+                        color: #172b4d;
                     }
 
                     .rq-new-one a,
-                    .footer-container a, .footer-container-snpw a {
+                    .footer-container a,
+                    .footer-container-snpw a {
                         color: #5e72e4;
                         text-decoration: none;
                     }
@@ -178,9 +208,69 @@ const GlobalStyle = () => {
                         margin-bottom: 40px;
                     }
 
-                    /* test code*/
-                    
-                    
+                    .forget-password-container {
+                        width: 100%;
+                    }
+
+                    /*set new password */
+
+                    .snpw-list-container {
+                        margin-left: 20px;
+                        font-size: 10px;
+                        color: #172b4d;
+                        margin-bottom: 12px;
+                        text-align: left;
+                        user-select: none;
+                    }
+
+                    .snpw-list-style li {
+                        list-style-type: disc;
+                        margin-left: 20px;
+                        line-height: 15px;
+                    }
+
+                    .pw-strenght {
+                        margin-left: 5px;
+                        font-size: 13px;
+                        font-weight: bold;
+                        font-style: italic;
+                        color: #8898aa;
+                    }
+
+                    .pw-strenght a {
+                        color: #2dce89;
+                        text-decoration: none;
+                    }
+
+                    .footer-container-snpw {
+                        margin-top: 40px;
+                        margin-bottom: 40px;
+                    }
+
+                    .btn-snpw {
+                        margin-top: 15px;
+                    }
+
+                    @media (max-width: 767px) {
+                        .forget-password-container {
+                            display: flex;
+                            flex-direction: column;
+                            text-align: center;
+                        }
+
+                        .fp-left-container,
+                        .snpw-left-container, .prc-left-container {
+                            width: 50%;
+                            height: 50%;
+                            margin-left: 0;
+                            margin-top: 2%;
+                        }
+
+                        .fp-right-container,
+                        .snpw-right-container, .prc-right-container {
+                            margin-right: 0;
+                        }
+                    }
                 `}
             />
         </>
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
-import GlobalStyle  from '../components/GlobalStyle/GlobalStyle'
+import GlobalStyle from "../components/GlobalStyle/GlobalStyle";
+import Head from "next/head";
 
 // import  ForgetPassword from '../components/ForgetPassword'
 function MyApp({ Component, pageProps }) {
-
-  return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
-  ) 
+    return (
+        <>
+            <Head>
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+                <title>NexStack</title>
+            </Head>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
